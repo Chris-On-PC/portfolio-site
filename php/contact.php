90f58bd1b7fe68d1095d9ed7f13194d4a179457d
Christiaan Viviers <ieatanithin@gmail.com>
2018-03-07 12:18:59 +0200

    Updates to files

---
+++ php/contact.php
@@ -0,0 +1,21 @@
+<?php
+ 
+    $to = "chrisviviers1@gmail.com"; 
+    $from = $_REQUEST['email']; 
+    $name = $_REQUEST['name']; 
+    $headers = "From: $from"; 
+    $subject = "You have a message sent from your site"; 
+ 
+    $fields = array(); 
+    $fields{"name"} = "name"; 
+    $fields{"email"} = "email"; 
+    $fields{"message"} = "message";
+ 
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+ 
+    //$send = mail($to, $subject, $body, $headers);
+
+    $send = mail($to, $subject, $body, $headers);
+    header("location:http://omnitek-fus.com"); 
+ 
+?>
